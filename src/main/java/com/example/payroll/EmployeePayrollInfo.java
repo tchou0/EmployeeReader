50bdf0e234ead76e88ab10886739bc571ba2e306
tchou0 <129346770+tchou0@users.noreply.github.com>
2023-04-25 22:10:12 -0400

    Update EmployeePayrollInfo.java

--- src/main/java/com/example/payroll/EmployeePayrollInfo.java
+++ src/main/java/com/example/payroll/EmployeePayrollInfo.java
@@ -1,33 +1,23 @@
-package com.example.payroll;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class EmployeePayrollInfo {
     public static void main(String[] args) {
-        //Read employee data from file
-        //generate pay information
-        //Print it out
         try {
             FileReader fileReader = new FileReader("PayDataFile");
             BufferedReader bufReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("Report.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            PrintWriter printWriter = new PrintWriter(bufferedWriter);
             String input;
-            System.out.println("Employee Gross Pay Information.");
+            printWriter.printf("Employee Gross Pay Information.\n");
+
+            //System.out.println("Employee Gross Pay Information.");
             while ((input = bufReader.readLine()) != null) {
-                String[] data =
-                        input.split("\\|");
-                Employee e =
-                        new Employee(data[0], data[1], Double.parseDouble(data[2]), Double.parseDouble(data[3]));
-                System.out.printf("%s %-40s $%.2f\n",e.getEmployeeId(),e.getName(),e.getGrossPay());
+                String[] data = input.split("\\|");
+                Employee e = new Employee(data[0],data[1],Double.parseDouble(data[2]),Double.parseDouble(data[3]));
+//                System.out.printf("%s %-40s $%.2f\n",e.getEmployeeId(),e.getName(),e.getGrossPay());
+                printWriter.printf("%s %-40s $%.2f\n", e.getEmployeeId(), e.getName(), e.getGrossPay());
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            printWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
